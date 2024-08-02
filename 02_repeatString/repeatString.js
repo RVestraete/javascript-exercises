c8b8923eb044596baf9c7fd2a67e974d39706e34
RVestraete <robin.vestraete@gmail.com>
2024-08-02 16:23:45 +0200

    Exercices 1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,repNum) {
+
+    if(repNum < 0){
+        return "ERROR"
+    }
+
+    let repString = "";
+
+    for (let i = 0; i < repNum; i++){
+        repString += string;
+    }
+
+    return repString;
+
 
 };
 
