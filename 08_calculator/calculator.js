3bd07c22edfe760e74db0e98363dea1ff18d864d
RVestraete <robin.vestraete@gmail.com>
2024-11-23 11:29:48 +0100

    Done until ex9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  return arr.reduce((a,b) => a + b,0);
 	
 };
 
-const sum = function() {
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b);
+};
+
+const power = function(a,p) {
+  let res = a;
+  for (let i = 1; i < p; i++){
+    res *= a;
+  }
+  return res
 	
 };
 
-const multiply = function() {
+const factorial = function(num) {
 
-};
+  let res = 1;
 
-const power = function() {
-	
-};
+  while (num > 0){
+    res *= num;
+    num -= 1;
+  }
 
-const factorial = function() {
-	
+  return res
+
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    string = string.toLowerCase().split('');
+
+    string = string.filter(item => /^[a-zA-Z0-9]+$/.test(item));
+
+    let palindrome = [...string];
+    palindrome.reverse();
+
+    string = string.join('');
+    palindrome = palindrome.join('');
+
+    return string === palindrome
 
 };
 
